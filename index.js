06bcd697940dda0d520258bd2f4ba4e2fad90312
Jewell-Micah <j.olumicah@gmail.com>
2021-11-27 19:31:23 +0000

    dockerising mongo

---
+++ index.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
