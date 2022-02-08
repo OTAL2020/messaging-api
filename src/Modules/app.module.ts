0c7e7fe5754d88141b43407994d2ab49ad3a489e
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2022-02-08 18:58:34 +0000

    Users-Login - Code Cleanup

--- src/Modules/app.module.ts
+++ src/Modules/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserController } from 'src/Controllers/user.controller';
 import { userService } from 'src/Services/user.service';
-import { AppController } from '../Controllers/app.controller';
-import { AppService } from '../Services/app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, UserController],
-  providers: [AppService, userService],
+  controllers: [UserController],
+  providers: [userService],
 })
 export class AppModule {}
