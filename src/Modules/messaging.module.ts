790ebc98d21b44720809ead54f9a8544eeb21d1f
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2022-02-15 17:14:19 +0000

    User-CRUD - Created messaging module for easier code readability

---
+++ src/Modules/messaging.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { UserController } from 'src/Controllers/user.controller';
+import { userService } from 'src/Services/user.service';
+
+@Module({
+  imports: [],
+  controllers: [UserController],
+  providers: [userService],
+})
+export class MessagingModule {}
