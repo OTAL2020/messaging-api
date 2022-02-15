5ed3f056ad032354dae0211143ca29149a0b86ea
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2022-02-15 17:16:25 +0000

    User-CRUD - Added DTO & validaton for full User CRUD

--- src/DTO/user.dto.ts
+++ src/DTO/user.dto.ts
@@ -17,3 +17,30 @@ export class userDTO {
     @IsInt({ message: "GuildId Field Must Contain A Interger" })
     guildId: number;
 }
+
+export class readuserDTO {
+    @IsNotEmpty({ message: "UserId Field is Required" })
+    @IsInt({ message: "UserId Field Must Contain A Interger"})
+    userId: number;
+}
+
+export class edituserDTO {
+    @IsNotEmpty({ message: "UserId Field is Required" })
+    @IsInt({ message: "UserId Field Must Contain A Interger"})
+    userId: number;
+
+    @IsString({ message: "Username Field Must Contain A String" })
+    username: string;
+
+    @IsInt({ message: "Level Field Must Contain A Interger" })
+    level: number;
+
+    @IsInt({ message: "GuildId Field Must Contain A Interger" })
+    guildId: number;
+}
+
+export class deleteuserDTO {
+    @IsNotEmpty({ message: "UserId Field is Required" })
+    @IsInt({ message: "UserId Field Must Contain A Interger"})
+    userId: number;
+}
