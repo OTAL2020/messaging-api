9b97ea812cc11d2a0b13f9d155ea550a8293b04e
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2022-02-15 17:15:41 +0000

    User-CRUD - imported MessagingModule to app module,  created messaging path

--- src/Modules/app.module.ts
+++ src/Modules/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { UserController } from 'src/Controllers/user.controller';
 import { userService } from 'src/Services/user.service';
+import { RouterModule } from '@nestjs/core';
+import { MessagingModule } from './messaging.module';
 
 @Module({
   imports: [],
   controllers: [UserController],
   providers: [userService],
+  imports: [
+    MessagingModule, RouterModule.register 
+    ([
+      { path: 'messaging', module: MessagingModule }
+    ]),
+  ]
 })
+export class AppModule {}
+  
 export class AppModule {}
