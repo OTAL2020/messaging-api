b84c83f216bbc7d86d0856e476c525738dadceaf
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2022-02-22 17:10:37 +0000

    UserDTO - Connected User Schema to MongoDB

--- src/Modules/messaging.module.ts
+++ src/Modules/messaging.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserController } from 'src/Controllers/user.controller';
 import { userService } from 'src/Services/user.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, userSchema } from 'src/Schema/user.schema';
+
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forFeature([{ name: User.name, schema: userSchema }])],
   controllers: [UserController],
   providers: [userService],
 })
