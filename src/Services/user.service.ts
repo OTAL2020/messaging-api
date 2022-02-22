a954e642207ba7f2b677f0363636a2bab9658d8d
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2022-02-22 17:11:52 +0000

    UserCRUD - Added UserCRUD services

--- src/Services/user.service.ts
+++ src/Services/user.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from "@nestjs/common";
-import { createuserDTO} from "src/DTO/user.dto";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { createuserDTO, readuserDTO, updateuserDTO} from "src/DTO/user.dto";
+import { User, UserDocument } from "src/Schema/user.schema";
 
 @Injectable()
 export class userService{
@@ -7,7 +10,27 @@ export class userService{
         @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
     ) {}
 
-    createUser(user: createuserDTO){
-        return user;
+    async createUser(user: createuserDTO): Promise<User>{
+        const createdUser = await this.userModel.create(user)
+        return createdUser;
+    }
+
+    async getAllUsers(): Promise<User[]> {
+        return this.userModel.find().exec();
+    }
+
+    async getUser(id: number): Promise<User> {
+        return this.userModel.findOne({ userId: id }).exec();
+    }
+
+    async updateUser(userdata : updateuserDTO){
+        await this.userModel.updateOne({userId:userdata.userId},userdata);
+    }
+
+    async deleteUser(id: number) {
+        const deletedUser = await this.userModel
+          .findOneAndDelete({ userId: id })
+          .exec();
+        return deletedUser;
     }
 }
