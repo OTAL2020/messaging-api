4a890240fac0b330117353863f5ca076b968b59f
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2022-02-22 17:11:22 +0000

    UserDTO - Added User Schema for use

--- src/Services/user.service.ts
+++ src/Services/user.service.ts
@@ -3,6 +3,9 @@ import { createuserDTO} from "src/DTO/user.dto";
 
 @Injectable()
 export class userService{
+    constructor(
+        @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+    ) {}
 
     createUser(user: createuserDTO){
         return user;
